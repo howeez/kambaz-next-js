30b7e386e256718f1de7cc0243b8ef620c2b30fa
Ezra Howe <howe.ez@northeastern.edu>
2025-10-31 23:23:24 -0400

    bug pt2

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import { Provider } from "react-redux";
+import store from "../store";
 import AddRedux from "./AddRedux";
 import CounterRedux from "./CounterRedux";
 import HelloRedux from "./HelloRedux";
 
 export default function ReduxExamples() {
   return(
+    <Provider store={store}>
     <div>
       <h2>Redux Examples</h2>
       <HelloRedux/>
       <CounterRedux/>
       <AddRedux/>
     </div>
+    </Provider>
   );
 };
